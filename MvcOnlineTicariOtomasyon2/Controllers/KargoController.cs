0f03f60b7e53e7990bb65ac2483894539ced80cd
Deniz Bayutmuş <denizbayutmus@gmail.com>
2022-01-27 13:35:15 +0300

    Mvc5 ile online ticari otomasyon
    Sql ~ Entity Framework ~ Code First ~ Html5 ~ Css3 ~ Bootstrap4

---
+++ MvcOnlineTicariOtomasyon2/Controllers/KargoController.cs
@@ -0,0 +1,58 @@
+﻿using MvcOnlineTicariOtomasyon2.Models.Siniflar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon2.Controllers
+{
+    public class KargoController : Controller
+    {
+        // GET: Kargo
+
+        Context c = new Context();
+        public ActionResult Index(string p)
+        {
+            var k = from x in c.kargoDetays select x;
+            if (!string.IsNullOrEmpty(p))
+            {
+                k = k.Where(y => y.TakipKodu.Contains(p));
+            }
+            return View(k.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult YeniKargo()
+        {
+            Random rnd = new Random();
+            string[] Karakterler = { "A", "B", "C", "D", "E", "F", "G", "H", "K" };
+            int k1, k2, k3;
+            k1 = rnd.Next(0, Karakterler.Length);
+            k2 = rnd.Next(0, Karakterler.Length);
+            k3 = rnd.Next(0, Karakterler.Length);
+            int s1, s2, s3;
+            s1 = rnd.Next(100, 1000);
+            s2 = rnd.Next(10, 99);
+            s3 = rnd.Next(10, 99);
+            string kod = s1.ToString() + Karakterler[k1] + s2 + Karakterler[k2] + s3 + Karakterler[k3];
+            ViewBag.takipkod = kod;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniKargo(KargoDetay d)
+        {
+            c.kargoDetays.Add(d);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KargoTakip(string id)
+        {
+
+            var degerler = c.KargoTakips.Where(x => x.TakipKodu == id).ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcOnlineTicariOtomasyon2.MvcApplication" Language="C#" %>
